2cfbd44b73f837b41be00fa6032f8eac0521693d
Sheraz Sarwar <sheraz786@gmail.com>
2018-08-14 11:04:55 +0100

    Update fixture difficulties and fix sorting method accordingly

--- src/Table.js
+++ src/Table.js
@@ -3,23 +3,54 @@ import ReactTable from 'react-table';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import 'react-table/react-table.css';
 
-const getColour = (difficulty) => {
-  if (difficulty < 13) {
+const getDefenceColour = (difficulty) => {
+  if (difficulty < 0.6) {
     return '#4CAF50';
-  } else if (difficulty < 25) {
+  } else if (difficulty < 1) {
     return '#8BC34A';
-  } else if (difficulty < 38) {
+  } else if (difficulty < 1.4) {
     return '#CDDC39';
-  } else if (difficulty < 50) {
+  } else if (difficulty < 1.8) {
     return '#FFEB3B';
-  } else if (difficulty < 63) {
+  } else if (difficulty < 2.2) {
     return '#FFC107';
-  } else if (difficulty < 75) {
+  } else if (difficulty < 2.6) {
     return '#FF9800';
-  } else if (difficulty < 88) {
+  } else if (difficulty < 3.0) {
     return '#FF5722';
   } else {
     return '#F44336';
+  }
+};
+
+const getOverallColour = (difficulty) => {
+  if (difficulty < 0.6) {
+    return '#F44336';
+  } else if (difficulty < 1) {
+    return '#FF5722';
+  } else if (difficulty < 1.4) {
+    return '#FF9800';
+  } else if (difficulty < 1.8) {
+    return '#FFC107';
+  } else if (difficulty < 2.2) {
+    return '#FFEB3B';
+  } else if (difficulty < 2.6) {
+    return '#CDDC39';
+  } else if (difficulty < 3.0) {
+    return '#8BC34A';
+  } else {
+    return '#4CAF50';
+  }
+};
+
+const getColour = (row, sort) => {
+  // Current 'overall' and 'attack' are treated the same
+  if (sort === 'overall' || sort === 'attack') {
+    return getOverallColour(row.overall);
+  }
+
+  if (sort === 'defence') {
+    return getDefenceColour(row.defence);
   }
 };
 
@@ -36,7 +67,7 @@ const CellText = ({ row }) => (
   </OverlayTrigger>
 );
 
-const getCell = (row) => {
+const getCell = (row, sort) => {
   if (!row) {
       return (<div style={{width: '100%', height: '100%', backgroundColor: '#aaa', padding: '4px', display: 'inline-block'}} />);
   }
@@ -48,7 +79,7 @@ const getCell = (row) => {
           style={{
             width: '50%',
             height: '100%',
-            backgroundColor: getColour(row[0].overall),
+            backgroundColor: getColour(row[0], sort),
             padding: '4px',
             display: 'inline-block',
             marginRight: '4px',
@@ -60,7 +91,7 @@ const getCell = (row) => {
           style={{
             width: '50%',
             height: '100%',
-            backgroundColor: getColour(row[1].overall),
+            backgroundColor: getColour(row[1], sort),
             padding: '4px',
             display: 'inline-block',
           }}
@@ -75,7 +106,7 @@ const getCell = (row) => {
     style={{
       width: '100%',
       height: '100%',
-      backgroundColor: getColour(row[0].overall),
+      backgroundColor: getColour(row[0], sort),
       padding: '4px',
       display: 'inline-block',
     }}
@@ -91,22 +122,21 @@ const Table = ({ data, gameweeks, range, sort }) => {
   const columns = Object.entries(gameweeks).filter(([gameweek, date]) => (date > now)).slice(0, range).map(value => ({
     Header: value[0],
     accessor: value[0].toLowerCase(),
-    Cell: row => (getCell(row.value)),
+    Cell: row => (getCell(row.value, sort)),
     sortMethod: (a, b) => {
       let first;
       let second;
-
       if (sort === 'overall') {
-        first = (a.length === 2) ? (parseInt(a[0].overall, 10) + parseInt(a[1].overall, 10)) : parseInt(a[0].overall, 10);
-        second = (b.length === 2) ? (parseInt(b[0].overall, 10) + parseInt(b[1].overall, 10)) : parseInt(b[0].overall, 10);
+        first = (a.length === 2) ? (parseFloat(a[0].overall, 10) + parseFloat(a[1].overall, 10)) : parseFloat(a[0].overall, 10);
+        second = (b.length === 2) ? (parseFloat(b[0].overall, 10) + parseFloat(b[1].overall, 10)) : parseFloat(b[0].overall, 10);
       }
       if (sort === 'attack') {
-        first = (a.length === 2) ? (parseInt(a[0].attack, 10) + parseInt(a[1].attack, 10)) : parseInt(a[0].attack, 10);
-        second = (b.length === 2) ? (parseInt(b[0].attack, 10) + parseInt(b[1].attack, 10)) : parseInt(b[0].attack, 10);
+        first = (a.length === 2) ? (parseFloat(a[0].attack, 10) + parseFloat(a[1].attack, 10)) : parseFloat(a[0].attack, 10);
+        second = (b.length === 2) ? (parseFloat(b[0].attack, 10) + parseFloat(b[1].attack, 10)) : parseFloat(b[0].attack, 10);
       }
       if (sort === 'defence') {
-        first = (a.length === 2) ? (parseInt(a[0].defence, 10) + parseInt(a[1].defence, 10)) : parseInt(a[0].defence, 10);
-        second = (b.length === 2) ? (parseInt(b[0].defence, 10) + parseInt(b[1].defence, 10)) : parseInt(b[0].defence, 10);
+        first = (a.length === 2) ? (parseFloat(a[0].defence, 10) + parseFloat(a[1].defence, 10)) : parseFloat(a[0].defence, 10);
+        second = (b.length === 2) ? (parseFloat(b[0].defence, 10) + parseFloat(b[1].defence, 10)) : parseFloat(b[0].defence, 10);
       }
 
       return first > second ? 1 : -1;
